c56e8643c3d01d4a143a4b808e457438b2a9303f
EzeG92 <gim24001@byui.edu>
2025-07-10 23:25:03 -0300

    adding post, put and delete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -9,7 +9,6 @@ const getAll = async (req, res) => {
   const result = await mongodb.getDb().db().collection('Contacts').find();
   result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
-
     res.status(200).json(contacts);
   });
 };
@@ -25,7 +24,61 @@ const getById = async (req, res) => {
   });
 };
 
+// Create a new contact
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const result = await mongodb.getDb().db().collection('Contacts').insertOne(contact);
+  if (result.acknowledged) {
+    res.status(201).send({ message: 'Contact created successfully', id: result.insertedId });
+  } else {
+    res.status(500).json({ message: 'Failed to create contact' });
+  }
+};
+
+// Update a contact by ID
+const updateContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection('Contacts')
+    .replaceOne({ _id: contactId }, contact);
+  if (result.modifiedCount > 0) {
+    res.status(200).send({ message: 'Contact updated successfully' });
+  } else {
+    res.status(404).json({ message: 'Contact not found or no changes made' });
+  }
+};
+
+// Delete a contact by ID
+const deleteContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('Contacts').deleteOne({ _id: contactId });
+  if (result.deletedCount > 0) {
+    res.status(200).send({ message: 'Contact delete successfully' });
+  } else {
+    res.status(404).json({ message: 'Contact not found or no changes made' });
+  }
+};
+
+// Export the controller functions to be used in routes
 module.exports = {
   getAll,
   getById,
+  createContact,
+  updateContact,
+  deleteContact,
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -10,4 +10,13 @@ router.get('/', conctactsController.getAll);
 // Get contact by ID
 router.get('/:id', conctactsController.getById);
 
+// Create a new contact
+router.post('/', conctactsController.createContact);
+
+// Update a contact by ID
+router.put('/:id', conctactsController.updateContact);
+
+// Delete a contact by ID
+router.delete('/:id', conctactsController.deleteContact);
+
 module.exports = router;
